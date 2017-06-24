749c1d6edf2a5132d277ae282a50e02f158ab51b
sneha <sneha.parekh11@gmail.com>
2017-06-24 15:19:11 -0400

    vaidation added

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,10 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  hasException: false,
+  
   actions:{
     signon(){
       this.get('model').save().then(()=>{
         this.transitionTo('candidate');
+      }).catch((adapterError)=>{
+        this.set('error',adapterError);
+        this.set('hasException',true);
       });
     }
   }
