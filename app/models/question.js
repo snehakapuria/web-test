ca554f03765776d146185357fd52a725b3e1befe
sneha <sneha.parekh11@gmail.com>
2017-06-24 16:03:22 -0400

    template updated

--- app/models/question.js
+++ app/models/question.js
@@ -2,5 +2,9 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   candidate_id:DS.attr('string'),
-  question1: DS.attr('string')
+  question1: DS.attr('string'),
+  question2: DS.attr('string'),
+  question3: DS.attr('string'),
+  question4: DS.attr('string'),
+  question5: DS.attr('string')
 });
